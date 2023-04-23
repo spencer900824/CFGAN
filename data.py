6c90f9bda5e5b09f80bd666461009b0ed5937bd4
spencer900824 <spencer900824@gmail.com>
2023-04-23 22:50:30 +0800

    with comment

--- data.py
+++ data.py
@@ -13,7 +13,6 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 
-
 def loadUseInfo(trainFile, splitMark):
     print(trainFile)
     UseInfo = pd.DataFrame(
@@ -109,67 +108,73 @@ def to_Vectors(trainSet, userCount, itemCount, userList_test, mode):
     print("Converting to vectors done....")
     return (torch.Tensor(trainVector)), torch.Tensor(testMaskVector), batchCount
 
-
 def pre_process_raw_data(input_file='raw.txt', output_file='welldone'):
+    # 建立存放結果的目錄
     dir_name = f'./welldones'
     os.makedirs(dir_name, exist_ok=True)
+
+    # 指定輸出檔案路徑
     output_file = f'{dir_name}/{output_file}'
 
-    # open the input and output files
+    # 讀取原始資料
     ss = []
     with open(input_file, 'r') as infile:
-        # iterate over each line in the input file
+        # 逐行讀取檔案
         for line in infile:
-            # split the line into fields
+            # 將每行資料以空白分隔，並只取前 5 欄
             fields = line.strip().split()[:-1]
             if '9999' not in fields:
-                # write the fields to the CSV file
+                # 將資料存入 ss
                 ss.append(fields)
             else:
+                # 如果該行資料中包含 '9999'，則跳過此行資料並輸出訊息
                 print(f"drop: {fields}")
+
+    # 將 ss 轉成 DataFrame
     df = pd.DataFrame(
         ss, columns='UserId, gender, age, merchantId, category'.split(', '))
+    # 指定新的欄位順序
     new_order = ["UserId", "merchantId", "gender", "age", "category"]
     df = df[new_order]
 
-    # create a mapping of UserId to sequential integer IDs
-    user_mapping = {user_id: i+1 for i,
-                    user_id in enumerate(df['UserId'].unique())}
+    # 建立 UserId 到序列整數 ID 的對應
+    user_mapping = {user_id: i+1 for i, user_id in enumerate(df['UserId'].unique())}
 
-    # create a mapping of MerchantId to sequential integer IDs
-    merchant_mapping = {merchant_id: i+1 for i,
-                        merchant_id in enumerate(df.iloc[:, 1].unique())}
+    # 建立 MerchantId 到序列整數 ID 的對應
+    merchant_mapping = {merchant_id: i+1 for i, merchant_id in enumerate(df.iloc[:, 1].unique())}
 
+    # 取得商品數量
     num_item = len(df.iloc[:, 1].unique())
 
-    # replace UserId and merchantId with their corresponding integer IDs
+    # 將 UserId 和 merchantId 換成對應的整數 ID
     df['UserId'] = df['UserId'].map(user_mapping)
     df["merchantId"] = df["merchantId"].map(merchant_mapping)
 
-    # write the transformed data to a new CSV file
-
+    # 將轉換後的資料寫入新檔案
     df.to_csv(f"./{output_file}.csv", index=False)
 
+    # 儲存每個用戶的個人資料，以備之後使用 (id 是先前產生的整數 ID )
     saved_user = set()
     with open(f'./{output_file}.user', 'w') as f:
         for s in df.values:
-            if s[0] not in saved_user:
+            if s[0] not in saved_user: # 跳過重複的用戶
+                # 為每個用戶建立記錄
                 n = [s[0], int(s[3]), s[2], int(0), int(0)]
                 ss = ''.join(str(i)+'|' for i in n)
                 f.write(ss[:-1] + '\n')
                 saved_user.add(s[0])
 
-    # slice to u*.base, u*.test
-    # mkdir
+    # 建立資料夾用以存放 train 和 test 資料
     u_dir_name = f"{dir_name}/u"
     os.makedirs(u_dir_name, exist_ok=True)
-
     for i in range(1, 6):
         train_data, test_data = train_test_split(
             df, test_size=0.2, random_state=i)
         train_data.to_csv(f"{u_dir_name}/u{i}.base",
-                          index=False, header=False, sep=',')
+                        index=False, header=False, sep=',')
         test_data.to_csv(f"{u_dir_name}/u{i}.test",
-                         index=False, header=False, sep=',')
+                        index=False, header=False, sep=',')
+
+    # 將資料切割為訓練集和測試集，並且將其寫入對應的檔案中
 
     return dir_name, num_item+1
